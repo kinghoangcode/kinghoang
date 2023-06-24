77ee6a58aec6f509163f5fa3967fdf3bad5c677a
hoangtc <hoang.tran@getz.co>
2023-06-24 14:19:48 +0700

    create your imformation popup

--- src/Presentations/WebApp/wwwroot/js/site.js
+++ src/Presentations/WebApp/wwwroot/js/site.js
@@ -2,3 +2,44 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(function () {
+
+    $('.coffee-order-btn').click(function () {
+        popupShow('#form-order-popup', () => {
+            $('#fullname').focus();
+        });
+    });
+
+    $('.coffee-submit-btn').click(function () {
+        popupHide('#form-order-popup');
+    });
+
+    $(document).on('click', '.popup-backdrop', () => {
+        popupHide('.king-popup');
+    });
+})
+
+const popupShow = (popupId, callback = null) => {
+    $(popupId).removeClass('popup-hide').addClass('popup-show');
+
+    $('body').append('<div class="popup-backdrop"></div>');
+
+    if (callback) {
+        callback();
+    }
+};
+
+
+const popupHide = (popupId, callback = null) => {
+    $(popupId).removeClass('popup-show').addClass('popup-hide');
+    setTimeout(() => {
+        $(popupId).removeClass('popup-hide');
+    }, 400);
+
+    $('body').find('.popup-backdrop').remove();
+
+    if (callback) {
+        callback();
+    }
+};
