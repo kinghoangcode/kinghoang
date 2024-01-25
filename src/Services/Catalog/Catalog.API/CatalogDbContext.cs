11f959c60d68ea0c00ca8b2893ae1b38d405d958
hoangtc <hoang.tran@getz.co>
2024-01-25 13:11:12 +0700

    update sql server

--- src/Services/Catalog/Catalog.API/CatalogDbContext.cs
+++ src/Services/Catalog/Catalog.API/CatalogDbContext.cs
@@ -10,7 +10,7 @@ namespace Catalog.API
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer("server=GETZ\\SQLEXPRESS;database=King.Catalog;trusted_connection=true;TrustServerCertificate=True;");
+            optionsBuilder.UseSqlServer("server=192.168.0.79,1433;database=King.Catalog;trusted_connection=true;TrustServerCertificate=True;");
         }
     }
 }
